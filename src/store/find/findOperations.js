138cfb8e2905f1957a4d4eec1d7891a1dc28ebd3
Nikolay Strelnikov <nikolaystrelnikov12345@gmail.com>
2022-03-14 18:46:07 +0200

    added text

--- src/store/find/findOperations.js
+++ src/store/find/findOperations.js
@@ -5,10 +5,8 @@ import {resetAction} from "./findActions";
 export const addTargetOperation = () => async (dispatch, getState) => {
     try {
         const find = getState().find;
-        const response =
             await axios.post(`${process.env.REACT_APP_BASE_URL}/base/${getCurrentDayWithoutDots()}.json`, find);
             dispatch(resetAction());
-
     } catch (e) {
         console.log(e)
     }
